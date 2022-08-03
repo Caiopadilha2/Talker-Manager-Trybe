12ba9d7ac2464c6a28931c9a7db503b13ef09077
Caio <caiopadilha@id.uff.br>
2022-08-02 23:55:04 -0300

    comentários

--- generateToken.js
+++ generateToken.js
@@ -4,4 +4,13 @@ function generateToken() {
   return crypto.randomBytes(8).toString('hex');
 }
 
+// const generateToken = (req, res, next) => {
+//   try {
+//     const token = crypto.randomBytes(8).toString('hex');
+//     return res.status(200).send({ token });
+//   } catch (err) {
+//     return next(err);
+//   }
+// };
+
 module.exports = { generateToken };
--- validateEmail.js
+++ validateEmail.js
@@ -1,26 +1,19 @@
-const validateEmail = (req, res) => {
-        const { email } = req.body;
+// const validateEmail = (req, res, next) => {
+//     try {
+//       const { email } = req.body;
 
-        if (!email) {
-          return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-        }
-        if (!(email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/))) {
-         return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-        }
-};
+//       if (!email) {
+//         return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+//       }
+//       if (!(email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/))) {
+//        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+//       }
+//       return next();
+//     } catch (err) {
+//       return next(err);
+//     }
+// };
 
-const validatePassword = (req, res) => {
-        const { password } = req.body;
-        if (!password) {
-          return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-        }
-        
-        if (password.length < 6) {
-          return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-        }
-};
-
-module.exports = {
-    validateEmail,
-    validatePassword,
-};
+// module.exports = {
+//   validateEmail,
+// };
